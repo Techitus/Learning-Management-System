78138232dcd4ab2d19b52d0ed816faf8b0dc2f35
Kamal Paudel <techitus07@gmail.com>
2025-02-27 10:09:46 +0545

    routing permission

--- app/(dashboard)/admin/layout.tsx
+++ app/(dashboard)/admin/layout.tsx
@@ -1,5 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
 import { DashboardLayout } from '@/components/layout/admin-dashboard-layout';
+import { Role } from '@/database/models/user.schema';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+ const {data:session,status}= useSession()
+ const router = useRouter()
+ useEffect(()=>{
+    if(status === 'loading'){
+      return;
+    }if(!session || session.user.role !== Role.Admin){
+     
+      router.push('/unauthorized')
+
+    }
+
+ },[session,status])
   return <DashboardLayout>{children}</DashboardLayout>;
 }
--- app/(dashboard)/teacher/layout.tsx
+++ app/(dashboard)/teacher/layout.tsx
@@ -1,5 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
 import { TeacherDashboardLayout } from '@/components/layout/teacher-dashboard-layout';
+import { Role } from '@/database/models/user.schema';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const {data:session,status}= useSession()
+   const router = useRouter()
+   useEffect(()=>{
+      if(status === 'loading'){
+        return;
+      }if(!session || session.user.role !== Role.Teacher){
+       
+        router.push('/unauthorized')
+  
+      }
+  
+   },[session,status])
   return <TeacherDashboardLayout>{children}</TeacherDashboardLayout>;
 }
