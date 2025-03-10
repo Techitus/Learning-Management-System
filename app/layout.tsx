52b8871464417ca361ed88c460ccd0b57f11c3c2
Kamal Paudel <techitus07@gmail.com>
2025-03-10 10:14:34 +0545

    ready for production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import "./globals.css"
-import { Inter } from "next/font/google"
 import type React from "react"
-
 import SessionWrapper from "@/components/SessionWrapper"
 import StoreProvider from "./StoreProvider"
 
-const inter = Inter({ subsets: ["latin"] })
 
 
 
@@ -18,7 +15,7 @@ export default function RootLayout({
     <SessionWrapper>
     <html lang="en" className="dark overflow-y-auto">
      
-      <body className={`${inter.className} custom-scroller bg-background  antialiased`}>
+      <body className={` custom-scroller bg-background  antialiased`}>
       <StoreProvider>
         {children}
      </StoreProvider>
