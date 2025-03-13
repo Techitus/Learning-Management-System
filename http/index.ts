3b29bfd6c200928d373dd731a2c5cd0e557deac7
Kamal Paudel <techitus07@gmail.com>
2025-03-13 18:00:57 +0545

    test deploy

--- http/index.ts
+++ http/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL : "https://elearn.kamalonweb.com/api", 
+    baseURL : "http://localhost:3000/api", 
     headers : {
         "Content-Type" : "application/json", 
         "Accept" : "application/json"
