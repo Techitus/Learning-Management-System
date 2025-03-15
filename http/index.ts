17fc33fcd2a83fc4d9a7f994da21665b3cff4b0d
Kamal Paudel <techitus07@gmail.com>
2025-03-15 11:33:44 +0545

    push

--- http/index.ts
+++ http/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL : "https://elearn.kamalonweb.com/api", 
+    baseURL : "https://elearnnnepal.vercel.app/api", 
     headers : {
         "Content-Type" : "application/json", 
         "Accept" : "application/json"
