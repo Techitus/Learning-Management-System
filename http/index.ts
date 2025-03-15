e3eee0cb13d3e570b70555d1bc165e6ccb9dc39b
Kamal Paudel <techitus07@gmail.com>
2025-03-15 11:18:20 +0545

    http issue fix

--- http/index.ts
+++ http/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL : "https://elearn.kamalonweb.com//api", 
+    baseURL : "https://elearn.kamalonweb.com/api", 
     headers : {
         "Content-Type" : "application/json", 
         "Accept" : "application/json"
